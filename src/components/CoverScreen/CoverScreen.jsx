17b91664537073dd7e4ecc95d769a45221c72f12
Rodrigo Daniel Hernandez Barrera <rodrigohernandezb7@gmail.com>
2024-01-16 15:43:03 -0600

    Add Cover Screen component with styles

---
+++ src/components/CoverScreen/CoverScreen.jsx
@@ -0,0 +1,28 @@
+
+import "./CoverScreen.css";
+
+const CoverScreen = ({score, onStartGame, duration}) => {
+    <div className="intro">
+        <h1 className="tltle">{score > -1 ? "Game Over" : "Pop a balloon 🎈"}</h1>
+        {score > -1 ? (
+            <p className="description">
+                {`You scored ${
+                    score === 0 ? "nothing" : `${score} ${score > 1 ? "hits" : "hit"}`
+                }`}
+            </p>
+        ) : (
+            <p className="description">
+                A simple {duration}-second balloon game built with React that i barely undestood.
+                Find the source here
+            </p>
+        )}
+
+        <div className="action">
+            <Button onClick={onStartGame} width={wide}>
+                {score > -1 ? "Play again" : "Start game"}
+            </Button>
+        </div>
+    </div>
+}
+
+export default CoverScreen;
